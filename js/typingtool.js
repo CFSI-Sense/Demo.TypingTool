afde8331ccd4f9d6d3c3596baf0e865a145e09c8
jschintz <james.schintz@gmail.com>
2016-03-01 12:02:33 -0500

    Update typingtool.js

--- js/typingtool.js
+++ js/typingtool.js
@@ -29,17 +29,17 @@ require( ["js/qlik"], function ( qlik ) {
 	
 	//get objects -- inserted here --
 	
-	app.getObject('QV01','RVjezMV');
-	app.getObject('QV02','eWyRUR');
-	app.getObject('QV03','LwtTYjq');
-	app.getObject('QV04','csXjXYA');
-	app.getObject('QV05','BVbpZv');
-	app.getObject('QV06','awfpES');
-	app.getObject('QV07','MyAuBm');
-	app.getObject('QV08','raUZcgG');
-	app.getObject('QV09','EcpuS');
-	app.getObject('QV10','ntpHbT');
-	app.getObject('QV11','SupJKJ');
+	app.getObject('QV03','RVjezMV');
+	app.getObject('QV04','eWyRUR');
+	app.getObject('QV05','LwtTYjq');
+	app.getObject('QV06','csXjXYA');
+	app.getObject('QV07','BVbpZv');
+	app.getObject('QV08','awfpES');
+	app.getObject('QV09','MyAuBm');
+	app.getObject('QV10','raUZcgG');
+	app.getObject('QV11','EcpuS');
+	app.getObject('QV12','ntpHbT');
+	app.getObject('QV13','SupJKJ');
 
 	//get listboxes
 	app.getObject('QV01','eSmfawp');
